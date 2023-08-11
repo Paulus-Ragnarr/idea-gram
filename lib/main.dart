3b0364ea93b8f8a091d400000a346db320f02c95
Paul Jairus Caberte <102200899+Paulus-Ragnarr@users.noreply.github.com>
2023-08-11 23:54:13 +0800

    Working and running code firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:idea_gram/app/app.bottomsheets.dart';
 import 'package:idea_gram/app/app.dialogs.dart';
@@ -6,8 +8,12 @@ import 'package:idea_gram/ui/views/app/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await setupLocator();
   setupDialogUi();
   setupBottomSheetUi();
   runApp(const Myapp());
+  
 }
